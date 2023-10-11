ed6543dcc3bd219bd2b87a6ed3ae5a490689549f
Mobilyst <2002derya2002@gmail.com>
2023-10-11 21:08:44 +0300

    General information was taken

---
+++ main.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char *argv[]) {
+	printf("Ad: Derya \n");
+	printf("Soyad: Telli \n");
+	printf("Meslek: Ogrenci \n");
+	printf("Semt: Yalinayak");
+	return 0;
+}
